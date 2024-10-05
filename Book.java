90ff45c0e387117c2c33ca392b5f62d2406cf480
LaVonne <168391894+lavonnepatoir@users.noreply.github.com>
2024-10-05 13:29:39 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,18 +1,22 @@
+//LaVonne Patoir, CEN3024C-14320,10/05/24
+//Software Development I
+//The purpose of this class is to define the elements of the Book object, and return a string listing an individual book
+
 public class Book {
-    //defines the element of the Book object
     String bookID;
     String author;
     String title;
 
-    public Book(String title, String author, String bookID) {
-    //creates a new object that stores bookID, book author, and book title
-        this.bookID = title;
+    public Book(String bookID, String title, String author) {
+    //Purpose: initializes new book object; Arguments: Strings title, author, bookID; Return Value: none
+        this.bookID = bookID;
+        this.title = title;
         this.author = author;
-        this.title = bookID;
     }
 
     public String toString() {
-    //returns a string of the book
+    //Purpose: returns a string of the Book object; Arguments: none; Return Value: a string
         return bookID + ", " + author + ", " + title;
     }
 }
+
