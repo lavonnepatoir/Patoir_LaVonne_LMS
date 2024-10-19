88a807eb48bee5476e405a415f25c4d4307ff48d
LaVonne <168391894+lavonnepatoir@users.noreply.github.com>
2024-10-19 12:12:40 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,6 +1,6 @@
 import java.time.LocalDate;
 
-//LaVonne Patoir, CEN3024C-14320,10/05/24
+//LaVonne Patoir, CEN3024C-14320,10/19/24
 //Software Development I
 //The purpose of this class is to define the elements of the Book object, and return a string listing an individual book
 
@@ -8,7 +8,7 @@ public class Book {
     String bookID;
     String author;
     String title;
-    String checkedInOut; // "checked in" or "checked out"
+    String bookStatus; // "checked in" or "checked out"
     LocalDate dueDate;
 
     public Book(String bookID, String title, String author) {
@@ -16,28 +16,27 @@ public class Book {
         this.bookID = bookID;
         this.title = title;
         this.author = author;
-        this.checkedInOut = "Checked in";
+        this.bookStatus = "Checked in";
         this.dueDate = null;
-    }
-
-    public boolean checkoutBook() {
-    //Purpose: changes the property of a book to being checkout out; Arguments: none; Return Value: boolean
-        if (this.checkedInOut.equals("Checked out")) {
-            return false;
-        }
-        this.checkedInOut = "Checked out";
-        this.dueDate = LocalDate.now().plusWeeks(4);
-        return true;
     }
 
     public void checkinBook() {
     //Purpose: changes the property of a book to being checked in; Arguments: none; Return Value: none
-        this.checkedInOut = "Checked in";
+        this.bookStatus = "Checked in";
         this.dueDate = null;
     }
 
     public String toString() {
     //Purpose: returns a string of the Book object; Arguments: none; Return Value: a string
         return bookID + ", " + author + ", " + title;
+    }
+
+    public boolean checkoutBook() {
+        if (this.bookStatus.equals("Checked out")) {
+            return false;
+        }
+        this.bookStatus = "Checked out";
+        this.dueDate = LocalDate.now().plusWeeks(4);
+        return true;
     }
 }
