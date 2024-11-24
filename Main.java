16985e0eb5c6d8ea8465b67af0c9f263c8798401
LaVonne <168391894+lavonnepatoir@users.noreply.github.com>
2024-11-24 12:46:53 -0500

    Update Main.java with Javadoc comments

--- Main.java
+++ Main.java
@@ -1,13 +1,21 @@
-//LaVonne Patoir, CEN3024C-14320, 11/14/24
-//Software Development I
-//The purpose of this Library Management System (LMS) is to add, remove, or list all books in a collection.
-//All actions (adding and removing of books) are committed through a GUI
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/*
+ * LaVonne Patoir, CEN3024C-14320, 11/24/24
+ * Software Development I
+ * The purpose of this Library Management System (LMS) is to add, remove, or list all books in a collection.
+ *
+ * All actions (adding and removing of books) are committed through a GUI
+ */
+
 public class Main {
+    /**
+     * Main method to launch the application.
+     *
+     * @param args command-line arguments
+     */
     public static void main(String[] args) {
         Library library = new Library();
         library.connect();
